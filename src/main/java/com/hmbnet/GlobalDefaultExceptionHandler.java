718c5162fa22b07bd313dcb9483b05d766b727b7
rhenderson07 <rhenderson07@gmail.com>
2017-03-03 16:50:51 -0500

    Improve error handling for server errors

--- src/main/java/com/hmbnet/GlobalDefaultExceptionHandler.java
+++ src/main/java/com/hmbnet/GlobalDefaultExceptionHandler.java
@@ -1,8 +1,12 @@
 package com.hmbnet;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -22,8 +26,14 @@ class GlobalDefaultExceptionHandler {
 
 	// Otherwise setup and send the user to a default error-view.
 	ModelAndView mav = new ModelAndView();
-	mav.addObject("exception", e);
-	mav.addObject("url", req.getRequestURL());
+	
+	HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+	
+	mav.addObject("timestamp", LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+	mav.addObject("status", status.value());
+	mav.addObject("error", status.getReasonPhrase());
+	mav.addObject("path", req.getRequestURL());
+	mav.addObject("message", e.getMessage());
 	mav.setViewName(DEFAULT_ERROR_VIEW);
 	return mav;
     }
--- src/main/java/com/hmbnet/controller/BasicErrorController.java
+++ src/main/java/com/hmbnet/controller/BasicErrorController.java
@@ -7,11 +7,13 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.ErrorAttributes;
 import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
@@ -19,51 +21,52 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 @RequestMapping("/error")
 public class BasicErrorController implements ErrorController {
 
-    private final ErrorAttributes errorAttributes;
+	private final ErrorAttributes errorAttributes;
 
-    @Autowired
-    public BasicErrorController(ErrorAttributes errorAttributes) {
-	this.errorAttributes = errorAttributes;
-    }
-
-    @Override
-    public String getErrorPath() {
-	return "/error";
-    }
-
-    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
-    @ResponseBody
-    public Map<String, Object> errorJson(HttpServletRequest request) {
-	return getErrorAttributeMap(request);
-    }
-
-    @RequestMapping(method = RequestMethod.GET, produces = "text/html")
-    public String errorHtml(HttpServletRequest request, Model model) {
-	model.addAllAttributes(getErrorAttributeMap(request));	
-	return getErrorPath();
-    }
-    
-    private Map<String, Object> getErrorAttributeMap(HttpServletRequest request){
-	Map<String, Object> body = getErrorAttributes(request, getTraceParameter(request));
-	String trace = (String) body.get("trace");
-	if (trace != null) {
-	    String[] lines = trace.split("\n\t");
-	    body.put("trace", lines);
+	@Autowired
+	public BasicErrorController(ErrorAttributes errorAttributes) {
+		this.errorAttributes = errorAttributes;
 	}
-	return body;
-    }
-    
-    private boolean getTraceParameter(HttpServletRequest request) {
-	String parameter = request.getParameter("trace");
-	if (parameter == null) {
-	    return false;
-	}
-	return !"false".equals(parameter.toLowerCase());
-    }
 
-    private Map<String, Object> getErrorAttributes(HttpServletRequest request, boolean includeStackTrace) {
-	RequestAttributes requestAttributes = new ServletRequestAttributes(request);
-	return errorAttributes.getErrorAttributes(requestAttributes, includeStackTrace);
-    }
+	@Override
+	public String getErrorPath() {
+		return "/error";
+	}
+
+	@RequestMapping(method = RequestMethod.GET, produces = "application/json")
+	@ResponseStatus(value=HttpStatus.INTERNAL_SERVER_ERROR)
+	@ResponseBody
+	public Map<String, Object> errorJson(HttpServletRequest request) {
+		return getErrorAttributeMap(request);
+	}
+
+	@RequestMapping(method = RequestMethod.GET, produces = "text/html")
+	public String errorHtml(HttpServletRequest request, Model model) {
+		model.addAllAttributes(getErrorAttributeMap(request));
+		return getErrorPath();
+	}
+
+	private Map<String, Object> getErrorAttributeMap(HttpServletRequest request) {
+		Map<String, Object> body = getErrorAttributes(request, getTraceParameter(request));
+		String trace = (String) body.get("trace");
+		if (trace != null) {
+			String[] lines = trace.split("\n\t");
+			body.put("trace", lines);
+		}
+		return body;
+	}
+
+	private boolean getTraceParameter(HttpServletRequest request) {
+		String parameter = request.getParameter("trace");
+		if (parameter == null) {
+			return false;
+		}
+		return !"false".equalsIgnoreCase(parameter);
+	}
+
+	private Map<String, Object> getErrorAttributes(HttpServletRequest request, boolean includeStackTrace) {
+		RequestAttributes requestAttributes = new ServletRequestAttributes(request);
+		return errorAttributes.getErrorAttributes(requestAttributes, includeStackTrace);
+	}
 
 }
